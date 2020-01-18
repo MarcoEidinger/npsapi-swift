c411fbc94f0a53d2dcd98cb1ca81765e82fd23fc
Marco Eidinger <marco.eidinger@sap.com>
2020-01-17 17:10:17 -0800

    more

---
+++ Sources/npsapi-swift/Model/States.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Eidinger, Marco on 1/16/20.
+//
+
+import Foundation
+
+enum States: String {
+    case californa = "CA"
+    case maine = "ME"
+}
--- Tests/npsapi-swiftTests/npsapi_swiftTests.swift
+++ Tests/npsapi-swiftTests/npsapi_swiftTests.swift
@@ -2,14 +2,37 @@ import XCTest
 @testable import npsapi_swift
 
 final class npsapi_swiftTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(npsapi_swift().text, "Hello, World!")
+
+    func testFetchParks() {
+        if #available(iOS 13.1, *) {
+            let api = NationalParkServiceApiForSwift(apiKey: "cxJhOzdPDG85b4HNpAbo932GsV7st7kPUQXNnd4W")
+            let expectation = XCTestExpectation(description: "Download Parks")
+            let publisher = api.fetchParks(by: ["acad"])
+            let subscription = publisher.sink(receiveCompletion: { (completion) in
+                switch completion {
+                case .finished:
+                    expectation.fulfill()
+                case .failure(let error):
+                    print(error)
+                    XCTFail()
+                }
+            }) { (parks) in
+                XCTAssertTrue(parks.count > 0, "We have parks")
+                guard let firstState = parks.first?.states.first else {
+                    XCTFail("No state")
+                    return
+                }
+                XCTAssertTrue(firstState == .maine, "We have a state")
+            }
+            XCTAssertNotNil(subscription)
+            XCTAssertNotNil(subscription)
+            wait(for: [expectation], timeout: 30.0)
+        } else {
+            XCTFail()
+        }
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testFetchParks", testFetchParks),
     ]
 }
