0245cda03a6bb02bc776c766184907508714a538
Marco Eidinger <marco.eidinger@sap.com>
2020-01-23 09:39:33 -0800

    adding ParkCodeConstants

--- Sources/npsapi-swift/Model/Park.swift
+++ Sources/npsapi-swift/Model/Park.swift
@@ -8,6 +8,40 @@
 import Foundation
 import CoreLocation
 
+/// Constants for the most important parks in the National Park system. List is not complete!
+public struct ParkCodeConstants {
+    /// Acadia National Park
+    public static let acadia = "acad"
+    /// Arches National Park
+    public static let arches = "arch"
+    /// Bryce Canyon National Park
+    public static let bryceCanyon = "brca"
+    /// Death Valley National Park
+    public static let deathValley = "deva"
+    /// Denali National Park
+    public static let denali = "dena"
+    /// Everglades National Park
+    public static let everglades = "ever"
+    /// Grand Canyon National Park
+    public static let grandCanyon = "grca"
+    /// Grand Teton National Park
+    public static let grandTeton = "grte"
+    /// Great Smokey Mountains National Park
+    public static let greatSmokeyMountains = "grsm"
+    /// Joshua Tree National Park
+    public static let joshuaTree = "jotr"
+    /// Katmai National Park
+    public static let katmai = "katm"
+    /// Rocky Mountainn National Park
+    public static let rockyMountains = "romo"
+    /// Yellowstone National Park
+    public static let yellowstone = "yell"
+    /// Yosemite National Park
+    public static let yosemite = "yose"
+    /// Zion National Park
+    public static let zion = "zion"
+}
+
 /// Requestable attribute of Park model
 public enum RequestableParkField: String, RequestableField {
     /// Park images
