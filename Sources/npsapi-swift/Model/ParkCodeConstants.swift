d78241b7f4bf4f8021eb47f70176dcbe235bd41d
Marco Eidinger <marco.eidinger@sap.com>
2020-01-23 16:08:03 -0800

    introduce ParkUnitDesignation as explicit type (enum)

---
+++ Sources/npsapi-swift/Model/ParkCodeConstants.swift
@@ -0,0 +1,42 @@
+//
+//  ParkCodeConstants.swift
+//  npsapi-swift
+//
+//  Created by Eidinger, Marco on 1/23/20.
+//
+
+import Foundation
+
+/// Constants for the most important parks in the National Park system. List is not complete!
+public struct ParkCodeConstants {
+    /// Acadia National Park
+    public static let acadia = "acad"
+    /// Arches National Park
+    public static let arches = "arch"
+    /// Bryce Canyon National Park
+    public static let bryceCanyon = "brca"
+    /// Death Valley National Park
+    public static let deathValley = "deva"
+    /// Denali National Park
+    public static let denali = "dena"
+    /// Everglades National Park
+    public static let everglades = "ever"
+    /// Grand Canyon National Park
+    public static let grandCanyon = "grca"
+    /// Grand Teton National Park
+    public static let grandTeton = "grte"
+    /// Great Smokey Mountains National Park
+    public static let greatSmokeyMountains = "grsm"
+    /// Joshua Tree National Park
+    public static let joshuaTree = "jotr"
+    /// Katmai National Park
+    public static let katmai = "katm"
+    /// Rocky Mountainn National Park
+    public static let rockyMountains = "romo"
+    /// Yellowstone National Park
+    public static let yellowstone = "yell"
+    /// Yosemite National Park
+    public static let yosemite = "yose"
+    /// Zion National Park
+    public static let zion = "zion"
+}
